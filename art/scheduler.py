fe8696cca7a2c485c8ca635a67f1367248a7e67e
lucastx <lucas@lucasteixeira.com>
2012-08-30 06:50:48 -0300

    simplified `Scheduler` methods.
    removed methods that were merely copied from the original scheme
    propagator and have no use at this development stage.
    
    modified methods to make use of python's object oriented features, in
    contrast to scheme's functional nature.
    
    merged `run_alerted` into `run`, and modified its loop to simply pop the
    propagators one by one from the queue until it is empty, instead of
    doing it in big buffers and recursing.

--- art/scheduler.py
+++ art/scheduler.py
@@ -9,6 +9,7 @@ Classes defined in this module:
 
 from contextlib import contextmanager
 from art.logging import debug, warn, error, info
+from art.util import SetQueue
 
 """
 Returns `value` it it is a list, `[value]` otherwise.
@@ -20,14 +21,6 @@ def listify(value):
         return [value]
 
 """
-Adds `thing` to `set_` and `list_` if it isn't in `set_` already.
-"""
-def order_preserving_insert(thing, set_, list_):
-    if thing not in set_:
-        set_.add(thing)
-        list_.append(thing)
-
-"""
 A scheduler that stores propagators in a queue ("alerts" them) and runs
 them until there are no propagators left.
 
@@ -37,58 +30,17 @@ will continue until the propagator network stabilizes.
 """
 class Scheduler:
     def __init__(self):
-        self.initialize()
+        self.alerted_propagators = SetQueue()
+        self.propagators_ever_alerted = SetQueue()
 
     """
     Initialize the scheduler, emptying its queues and registers.
     """
     def initialize(self):
         debug("Initializing scheduler")
+        self.alerted_propagators.clear()
+        self.propagators_ever_alerted.clear()
 
-        self.clear_alerted_propagators()
-
-        self.abort_process = False
-        self.last_value_of_run = "done"
-        self.propagators_ever_alerted = set()
-        self.propagators_ever_alerted_list = []
-
-    """
-    Returns `True` if there are any alerted propagators in the queue; `False` otherwise.
-    """
-    @property
-    def any_propagators_alerted(self):
-        return len(self.alerted_propagators) > 0
-
-    """
-    Empties the queue of alerted propagators.
-    """
-    def clear_alerted_propagators(self):
-        debug("Clearing alerted propagators")
-        self.alerted_propagators = set()
-        self.alerted_propagators_list = []
-
-    def ordered_key_list(self, set_, list_):
-        return list_[::]
-
-    def alert_all_propagators(self):
-        self.alert_propagators(
-                ordered_key_list(self.propagators_ever_alerted,
-                    self.propagators_ever_alerted_list))
-
-    """
-    Returns the alerted propagators.
-    """
-    @property
-    def the_alerted_propagators(self):
-        return self.ordered_key_list(self.alerted_propagators,
-            self.alerted_propagators_list)
-
-    @staticmethod
-    @contextmanager
-    def process_abortion(k):
-        old_abort_process, self.abort_process = self.abort_process, k
-        yield
-        self.abort_process = old_abort_process
 
     """
     Alerts all propagators in `propagators`.
@@ -102,32 +54,16 @@ class Scheduler:
         debug("Alerting propagators: {0}".format(propagators))
         for p in listify(propagators):
             assert callable(p), "Alerting a non-procedure"
-            order_preserving_insert(p, self.propagators_ever_alerted,
-                    self.propagators_ever_alerted_list)
-            order_preserving_insert(p, self.alerted_propagators,
-                    self.alerted_propagators_list)
+            self.propagators_ever_alerted.add(p)
+            self.alerted_propagators.add(p)
 
     """
     Pops and runs alerted propagators from the queue them until it is
     empty.
     """
-    def run_alerted(self):
-        temp = self.the_alerted_propagators
-        debug("Running alerted propagators: {0}".format(temp))
-        self.clear_alerted_propagators()
-        for propagator in temp:
-            propagator()
-        if self.any_propagators_alerted:
-            debug("More propagators alerted; let's run them")
-            self.run_alerted()
-
-    """
-    Starts running the scheduler's queue.
-    """
     def run(self):
         debug("Running scheduler")
-        if self.any_propagators_alerted:
-            self.last_value_of_run = self.run_alerted()
-        else:
-            debug("Scheduler done")
-            return self.last_value_of_run
+        while len(self.alerted_propagators):
+            propagator = self.alerted_propagators.pop()
+            propagator()
+        debug("Scheduler done")

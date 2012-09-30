caf9394666a5d1488b76ee40c6e66c635cfdf145
lucastx <lucas@lucasteixeira.com>
2012-09-29 23:05:26 -0300

    simple tests for supported module

---
+++ propagator/tests/test_supported.py
@@ -0,0 +1,34 @@
+import unittest
+
+from propagator import scheduler
+from propagator.network import Cell, Propagator, merge
+from propagator.primitives import *
+from propagator.supported import Support, Supported
+
+class TestCaseWithScheduler(unittest.TestCase):
+    def setUp(self):
+        scheduler.initialize()
+
+
+class SupportTestCase(unittest.TestCase):
+    def test_more_informative_than(self):
+        s1 = Support(["source1", "source2", "source3"])
+        s2 = Support(["source1", "source2"])
+        self.assertTrue(s2.more_informative_than(s1))
+
+class SupportedTestCase(TestCaseWithScheduler):
+    def test_adder_exact(self):
+        c1 = Cell(content=Supported(3))
+        c2 = Cell(content=Supported(4))
+        c3 = Cell()
+        adder(c1, c2, c3)
+        scheduler.run()
+        self.assertEqual(c3.content, Supported(7))
+
+    def test_multiplier_exact(self):
+        c1 = Cell(content=Supported(3))
+        c2 = Cell(content=Supported(4))
+        c3 = Cell()
+        multiplier(c1, c2, c3)
+        scheduler.run()
+        self.assertEqual(c3.content, Supported(12))
